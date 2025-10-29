0e3706cb1c9794cdd109d0912f5f29c903ee9925
Alisadek1 <alisadek700@gmail.com>
2025-10-29 18:25:43 +0300

    Revert "Add all project files"
    This reverts commit 31dee7346c9f29a45c928ef49b49122af241fec1.

--- apple-store/admin/includes/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../../config/database.php';
-require_once __DIR__ . '/../../includes/functions.php';
-require_once __DIR__ . '/../../includes/lang.php';
-require_once __DIR__ . '/security-middleware.php';
-
-// Enhanced admin access validation with security middleware
-validateAdminAccess();
